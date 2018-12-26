ba63be7086ce5ba67627d03f2a91ba9c50ebcfcb
David Goligorsky <d.goligorsky@gmail.com>
2018-12-25 21:04:18 -0500

    commit to github

--- js/drawer.js
+++ js/drawer.js
@@ -1,10 +1,10 @@
 // set up a renderer
 const renderer = new THREE.WebGLRenderer({
-    antialias: true
+    antialias: true,
+    alpha: true
 })
 renderer.setSize(window.innerWidth, window.innerHeight)
 renderer.setPixelRatio(window.devicePixelRatio)
-renderer.setClearColor(0x333333, 1)
 
 // choose the element to add the renderer to
 const section = document.querySelector("section")
@@ -12,6 +12,7 @@ section.appendChild(renderer.domElement)
 
 // create a SCENE
 const scene = new THREE.Scene()
+scene.background = new THREE.Color(0xF4E4CF)
 
 // create a CAMERA
 const camera = new THREE.PerspectiveCamera( 110, window.innerWidth / window.innerHeight, 0.1, 5000) // fov, aspect ratio, near clipping, far clipping
@@ -24,6 +25,42 @@ const light = new THREE.DirectionalLight(0xffffff, 1)
 light.position.set(0,0,-1)
 scene.add(light) 
 
+// using JS promises. Creating a new promise function that is passed the mtl and obj asset. On resolve (when the files are successfully loaded) the function returns the object. Now you can use the loadFiles() function and add on a .then() method that easily adds new obj/mtls to the scene.
+const loadFiles = function(mtlUrl, objUrl) {
+    return new Promise((resolve, reject) => {
+        const objLoader = new THREE.OBJLoader()
+        const mtlLoader = new THREE.MTLLoader()
+        
+        mtlLoader.load(mtlUrl, function(materials) {
+            objLoader.setMaterials(materials)
+            objLoader.load(objUrl, function(obj) {
+                resolve(obj)
+            })
+        })
+
+    })
+}
+
+let dg = null
+let dgColor = "#0033cc"
+loadFiles("assets/dg.mtl", "assets/dg.obj").then(function(obj) {
+    obj.rotateY(Math.PI)
+    obj.position.y = -5
+    obj.position.z = -40
+
+    // go through the model to update material
+    const material = new THREE.MeshLambertMaterial({
+        color: new THREE.Color(dgColor),
+        reflectivity: 1
+    })
+    obj.traverse(child => {
+        child.material = material
+    })
+
+    dg = obj
+    scene.add(dg)
+})
+
 // hold some data about shapes being added
 const shapes = []
 
@@ -33,15 +70,14 @@ const animate = function() {
     requestAnimationFrame(animate)
 
     // move the camera forward in space
-    camera.position.setZ(camera.position.z + 1)
+    // camera.position.setZ(camera.position.z + 1)
 
     // if you want to rotate the shapes each frame
     shapes.forEach(shape => {
         shape.rotateX(0.01)
-        // // move the objects toward the camera
-        // shape.position.setZ(shape.position.z - 1)
     })
 
+    dg.rotateY(0.01)
 }
 // and kick off the animation
 animate()
@@ -71,14 +107,15 @@ const createShape = function(x, y) {
     })
 
     const shape = new THREE.Mesh(geometry, material)
+    
 
     shape.position.set(
         (window.innerWidth / 2) - x, 
         (window.innerHeight / 2) - y, 
         camera.position.z + 500 // this makes sure you're placing objects in front of camera
         )
-    shape.rotateX(0.5)
-    shape.rotateZ(0.5)
+    shape.rotateX(1.5)
+    shape.rotateZ(1.5)
 
     // add the shape to the scene and the list (array) of "shapes"
     shapes.push(shape)
